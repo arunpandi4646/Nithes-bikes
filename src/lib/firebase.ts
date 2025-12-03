f6cee702c5903c0cf3ede7c12cc53b9356433583
Arun Pandi <itsmearunpandi@gmail.com>
2025-12-03 14:50:18 +0000

    this is uid for admin login:zMW1z5cRS1QbreKX16h1jNzoEm02

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAYjTRGQouFgSJJ689XwKN14fFfsMfK26M",
   authDomain: "studio-398059978-802b3.firebaseapp.com",
   projectId: "studio-398059978-802b3",
-  storageBucket: "studio-398059978-802b3.firebasestorage.app",
+  storageBucket: "studio-398059978-802b3.appspot.com",
   messagingSenderId: "783407561608",
   appId: "1:783407561608:web:846006efb8aedb500ad83d"
 };

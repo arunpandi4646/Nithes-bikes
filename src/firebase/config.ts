e45413e212294bbb3dda501a0705942f7fd04209
Arun Pandi <itsmearunpandi@gmail.com>
2025-12-04 17:43:55 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAYjTRGQouFgSJJ689XwKN14fFfsMfK

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-398059978-802b3",
-  "appId": "1:783407561608:web:846006efb8aedb500ad83d",
-  "apiKey": "AIzaSyAYjTRGQouFgSJJ689XwKN14fFfsMfK26M",
-  "authDomain": "studio-398059978-802b3.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "783407561608"
+  apiKey: "AIzaSyAYjTRGQouFgSJJ689XwKN14fFfsMfK26M",
+  authDomain: "studio-398059978-802b3.firebaseapp.com",
+  databaseURL: "https://studio-398059978-802b3-default-rtdb.firebaseio.com",
+  projectId: "studio-398059978-802b3",
+  storageBucket: "studio-398059978-802b3.appspot.com",
+  messagingSenderId: "783407561608",
+  appId: "1:783407561608:web:846006efb8aedb500ad83d"
 };
